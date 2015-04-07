2ecbeb4b3b39d863630e80e2d34d32de7202aae9
Yoshitaka Hashimoto <yhashimoto@sonix.asia>
2015-04-07 19:15:44 +0900

    20150407

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,23 @@
+# coding: utf-8
+class BlogsController < ApplicationController
+
+  #トップページの定義
+  #トップページには、投稿へのリンク・及び全ての記事の表示全ての記事に置いて編集ページ（内容の編集・削除）を行えるようにする。
+  def index
+    @blog = Blog.all
+  end
+
+  #作成画面
+  #作成画面には、タイトルと本文を入力できるようにする。
+  def new
+    @blog = Blog.new
+  end
+  
+  def create
+    @blog = Blog.new(params[:blog].permit(:title) ,
+                     params[:blog].permit(:title))
+    @blog.save
+    redirect_to blogs_path
+  end
+      
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+# coding: utf-8
 Rails.application.routes.draw do
+  resources :blogs
+  
+  #トップページをblogs.indexへ変更
+  root 'blogs#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
