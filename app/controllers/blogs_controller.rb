49c5f473f4aa6c34a91e1a154417bb8a1ea7cc96
Yoshitaka Hashimoto <yhashimoto@sonix.asia>
2015-04-08 15:15:27 +0900

    バグチェックなしの状態

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,41 +1,47 @@
 # coding: utf-8
 class BlogsController < ApplicationController
   
-  #
+  #before_actionの設定
   before_action :set_blog, only: [:show, :destroy, :edit, :update]
+  
   #トップページの定義
-  #トップページには、投稿へのリンク・及び全ての記事の表示全ての記事に置いて編集ページ（内容の編集・削除）を行えるようにする。
   def index
     @blog = Blog.all
   end
   
-  #作成画面
-  #作成画面には、タイトルと本文を入力できるようにする。
+  #記事作成ページ
   def new
     @blog = Blog.new
   end
-  
+
+  #記事作成create
   def create
-    #blogの情報を取得し、投稿データを保存する。
     @blog = Blog.new(blogs_params)
-    @blog.save
-    redirect_to blogs_path
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render 'new'
+    end
   end
 
+  #記事編集ページ
   def edit
     
   end
 
-  
+  #記事編集update
   def update
+    
     @blog.update(blogs_params)
     redirect_to blogs_path
   end
-  
+
+  #全文表示ページ
   def show
     
   end
 
+  #記事削除コマンド
   def destroy
     @blog.destroy
     redirect_to blogs_path
@@ -43,12 +49,19 @@ class BlogsController < ApplicationController
   
   private
 
+  #記事のidを取得するメソッド
   def set_blog
     @blog = Blog.find(params[:id])
   end
 
+  #入力されたパラメータを保持する。
   def blogs_params
     params[:blog].permit(:title, :main)
+  end
+
+  #全文表示  
+  def show
+    
   end
   
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class Blog < ActiveRecord::Base
-  validates :title, :main, presence: true
+  validates :title, :main, presence:{ message: "タイトルもしくは本文が空です。"}
 end
